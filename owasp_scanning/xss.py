f3836c010b0e57cbeda3e2876c3da46a92ffdcc0
Mohamed Abdelfttah <110192568+3bdelfttah@users.noreply.github.com>
2023-12-22 22:47:02 +0200

    Update xss.py

--- owasp_scanning/xss.py
+++ owasp_scanning/xss.py
@@ -3,29 +3,31 @@ import sys
 def xss_scan(urls_file):
     print("[~] Scanning urls for xss . . .")
     with open(urls_file, 'r') as urls:
-    	xss_urls=[i.strip() for i in urls.readlines() if "=" in i]
+        xss_urls=[i.strip() for i in urls.readlines() if "=" in i]
     xss_urls_path='root/WebArmor/owasp_scanning/utils/xss_urls.txt'
     with open(xss_urls_path,'w') as f:
-     	for i in xss_urls:
-     		f.write(i)
+        for i in xss_urls:
+            f.write(i)
     try:
-       						with open (xss_urls_path,'r') as o:
-       							vuln_urls=[i for i in o.readlines()]
+        with open (xss_urls_path,'r') as o:
+            vuln_urls=[i for i in o.readlines()]
     except FileNotFoundError:
-       					print("No xss urls file was found ! ")
-       					sys.exit()
+        print("No xss urls file was found ! ")
     for xurl in vuln_urls:
-       try:
-       				command = f"dalfox url '{xurl}' -o 'root/WebArmor/DATA_FOLDER/owasp_scanning/xss.txt' --waf-evasion --deep-domxss --mining-dict --mining-dom"
-       				process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-       				process.wait()
+        try:
+            with open('/root/WebArmor/config.yaml', 'r') as config_file:
+                config = yaml.safe_load(config_file)
+       		output_file=config["OWASP"]["XSS_OUTPUT_PATH"]
+       		command = f"dalfox url '{xurl}' -o '{output_file}' --waf-evasion --deep-domxss --mining-dict --mining-dom"
+       		process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+       		process.wait()
        except Exception as e:
-       	print(f"Error executing command '{command}': {e}")
+            print(f"Error executing command '{command}': {e}")
      	
 if __name__ == "__main__":
-       if len(sys.argv) != 2:
-       	print("Usage: python script.py input_file")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py input_file")
        	sys.exit(1)
-       input_file = sys.argv[1]
-       xss_scan(input_file)
-       print("Output saved to: root/WebArmor/DATA_FOLDER/owasp_scanning/xss.txt")
+    input_file = sys.argv[1]
+    xss_scan(input_file)
+    print("Output saved to: root/WebArmor/DATA_FOLDER/owasp_scanning/xss.txt")
