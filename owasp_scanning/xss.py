04ee5386b566e8a480fbc594e59666dac7cd6d0a
Mohamed Abdelfttah <110192568+3bdelfttah@users.noreply.github.com>
2023-12-29 21:13:39 +0200

    Update xss.py

--- owasp_scanning/xss.py
+++ owasp_scanning/xss.py
@@ -1,37 +1,47 @@
 import subprocess
-import sys
 import yaml
+import sys
 
-def xss_scan(urls_file):
+def xss_scan():
+    with open('/root/WebArmor/config.yaml', 'r') as config_file:
+        config = yaml.safe_load(config_file)
+
+    urls_file_path = config['CRAWLING_URL_ENUMERATION']['OUTPUT_FOLDER_PATH']
     print("[~] Scanning urls for xss . . .")
-    with open(urls_file, 'r') as urls:
-        xss_urls = [i.strip() for i in urls.readlines() if "=" in i]
-    xss_urls_path = 'root/WebArmor/owasp_scanning/utils/xss_urls.txt'
-    with open(xss_urls_path, 'w') as f:
-        for i in xss_urls:
-            f.write(i)
+    
+    with open(urls_file_path, 'r') as f:
+        urls = list(set([i.strip() for i in f.readlines() if "=" in i and "redir=" not in i and "r=" not in i]))
+        
+    uniq_params = dict()
+    for i in urls:
+        uniq_params.update({i[:i.index('=') + 1]: i[i.index('=') + 1:]})
+    clean_urls = [key + value for key, value in uniq_params.items()]
+    
+    clean_urls_path = '/root/WebArmor/owasp_scanning/utils/cleanXss.txt'
+    
+    with open(clean_urls_path, 'w') as f:
+        for i in clean_urls:
+            f.write(i+'\n')
 
     try:
-        with open(xss_urls_path, 'r') as o:
-            vuln_urls = [i for i in o.readlines()]
-    except FileNotFoundError:
-        print("No xss urls file was found ! ")
+        for xurl in clean_urls[:3]:
+            try:
+                out = config['OWASP']["XSS_OUTPUT_PATH"]
+                command = f"~/go/bin/./dalfox url '{xurl}' -o '{out}'"
+                
+                process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                process.wait()
+            except Exception as e:
+                print(f"Error executing command '{command}': {e}")
 
-    for xurl in vuln_urls:
-        try:
-            with open('/root/WebArmor/config.yaml', 'r') as config_file:
-                config = yaml.safe_load(config_file)
-            output_file = config["OWASP"]["XSS_OUTPUT_PATH"]
-            command = f"dalfox url '{xurl}' -o '{output_file}' --waf-evasion --deep-domxss --mining-dict --mining-dom"
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            process.wait()
-        except Exception as e:
-            print(f"Error executing command '{command}': {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py input_file")
         sys.exit(1)
+    
     input_file = sys.argv[1]
-    xss_scan(input_file)
+    xss_scan()
     print("Output saved to: root/WebArmor/DATA_FOLDER/owasp_scanning/xss.txt")
