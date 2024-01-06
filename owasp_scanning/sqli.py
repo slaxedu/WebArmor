2eb54e5efa139cbc22e1c1e5d7d8ff46defc2f6b
Mohamed Abdelfttah <110192568+3bdelfttah@users.noreply.github.com>
2024-01-06 22:53:30 +0200

    Update sqli.py

--- owasp_scanning/sqli.py
+++ owasp_scanning/sqli.py
@@ -2,8 +2,12 @@ import subprocess
 import sys
 import yaml
 
-def sqli_scan(urls_file_path):
+def scan():
     print("\n[~] Scanning for SQL injection . . .")
+    with open('/root/WebArmor/config.yaml', 'r') as config_file:
+        config = yaml.safe_load(config_file)
+    output_folder = config["OWASP"]["SQLI_OUTPUT_PATH"]
+    urls_file_path = config['CRAWLING_URL_ENUMERATION']['OUTPUT_FOLDER_PATH']
 
     try:
         with open(urls_file_path, 'r') as f:
@@ -12,7 +16,7 @@ def sqli_scan(urls_file_path):
         for i in urls:
             uniq_params.update({i[:i.index('=')+1]: i[i.index('=')+1:]})
         clean_urls = [key + value for key, value in uniq_params.items()]
-        clean_urlsfile_path = 'root/WebArmor/owasp_scanning/utils/Sql_URLS.txt'
+        clean_urlsfile_path = '/root/WebArmor/owasp_scanning/utils/Sql_URLS.txt'
         with open(clean_urlsfile_path, 'w') as f:
             for i in clean_urls:
                 f.write(i + '\n')
@@ -21,18 +25,9 @@ def sqli_scan(urls_file_path):
         sys.exit()
 
     try:
-        with open('/root/WebArmor/config.yaml', 'r') as config_file:
-            config = yaml.safe_load(config_file)
-        output_folder = config["OWASP"]["SQLI_OUTPUT_PATH"]
         command = f"sqlmap -m '{clean_urlsfile_path}' --dbs -f --batch --output-dir='{output_folder}' --results-file='/dev/null'"
         process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         process.wait()
         print(f"Output saved to : {output_folder}")
     except Exception as e:
         print(f"Error while starting sqlmap: {e}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit(1)
-
-    sqli_scan(sys.argv[1])
