f391768e3105c56665945741a732f5040097489f
Mohamed Abdelfttah <110192568+3bdelfttah@users.noreply.github.com>
2024-01-23 23:00:10 +0200

    Update subs_technolgoies.py

--- asset_discovery/subs_technolgoies.py
+++ asset_discovery/subs_technolgoies.py
@@ -1,10 +1,10 @@
+from Wappalyzer import Wappalyzer, WebPage
 import yaml
 from halo import Halo
-from requests.exceptions import SSLError
-import subprocess
-import sys
 
 def detect_technologies(config):
+    wappalyzer = Wappalyzer.latest()
+
     results = {}
     probed_subdomains_file_path = config["ASSET_DISCOVERY"]["PROBED_SUBDOMAINS_FILE_PATH"]
     technologies_file_path = config["ASSET_DISCOVERY"]["TECHNOLOGIES_FILE_PATH"]
@@ -17,28 +17,18 @@ def detect_technologies(config):
         with open(probed_subdomains_file_path, 'r') as file:
             subdomains = [line.strip() for line in file.readlines()]
 
-        with Halo(text="Detecting technologies", spinner="dots"):
-            for subdomain in subdomains:
-                try:
-                    # Use subprocess to call webanalyze tool
-                    command = [
-                        "webanalyze",
-                        "-host", subdomain,
-                        "-output", "json",  # Change output format if needed
-                        "-silent"  # Add other flags as needed
-                    ]
-                    result = subprocess.run(command, capture_output=True, text=True)
-
-                    # Parse the JSON output
-                    technologies = result.stdout.strip()
-                    results[subdomain] = technologies
-                except SSLError as ssl_error:
-                    # Suppress SSL errors
-                    pass
-                except Exception as e:
-                    print(f"Error analyzing {subdomain}: {e}", file=sys.stderr)
+        with open(technologies_file_path, 'w') as output:
+            with Halo(text="Detecting technologies", spinner="dots"):
+                for subdomain in subdomains:
+                    try:
+                        webpage = WebPage.new_from_url(subdomain)
+                        technologies = wappalyzer.analyze(webpage)
+                        results[subdomain] = technologies
+                        output.write(f"{subdomain} => {technologies}\n")
+                    except Exception:
+                        pass
     except Exception as e:
-        print(f"Error: {e}", file=sys.stderr)
+        print(f"Error: {e}")
 
     return results
 
@@ -50,14 +40,11 @@ def subs_technologies():
     with open(config_path, "r") as config_file:
         config = yaml.safe_load(config_file)
 
-    # Run the function and save the results to the variable
+    # Run the function and save the results
     subdomains_with_technologies = detect_technologies(config)
 
-    # Store the results in the specified variable in your config
-    config["ASSET_DISCOVERY"]["TECHNOLOGIES_RESULTS"] = subdomains_with_technologies
-
     # Print a message indicating the completion of the script
-    print("Done with detecting technologies.\n")
+    print("Done with detecting technologies.")
 
 # Call the main function if the script is executed directly
 if __name__ == "__main__":
